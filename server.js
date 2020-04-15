91a688579093daad15d86da6a3b9bfff7eeb8f2c
Shashang <51124608+shashang29@users.noreply.github.com>
2020-04-15 13:55:47 +0545

    updating our code to be pwa compliant

--- server.js
+++ server.js
@@ -19,14 +19,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(compression);
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+
 }
 
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server is running on port ' + port);
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.get('/service-worker.js', (req, res) => {
