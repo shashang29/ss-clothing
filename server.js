4d82c6e4fa455f9042e62f3bb50a62c933c99565
Shashang <51124608+shashang29@users.noreply.github.com>
2020-04-16 17:32:14 +0545

    updating our code to be pwa compliant

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
-const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -11,27 +10,22 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(compression);
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log('Server is running on port ' + port);
-});
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
-app.get('/service-worker.js', (req, res) => {
-  res.sendfile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+  console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {
