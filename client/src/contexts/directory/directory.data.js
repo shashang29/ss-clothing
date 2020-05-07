c65065c1eb6e4fac83700e56c747b48e19e8df0c
Shashang <51124608+shashang29@users.noreply.github.com>
2020-05-07 22:52:34 +0545

    styling changes

--- client/src/contexts/directory/directory.data.js
+++ client/src/contexts/directory/directory.data.js
@@ -2,32 +2,32 @@ const DIRECTORY_DATA = {
     sections: [
         {
             title: 'hats',
-            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            imageUrl: 'https://i.ibb.co/phPCr6y/hats2.jpg',
             id: 1,
             linkUrl: 'shop/hats'
         },
         {
             title: 'jackets',
-            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            imageUrl: 'https://i.ibb.co/R2fNJ19/Baseball-Uniform-Coat-Male-Jaqueta-Masculino-Bomber.jpg',
             id: 2,
             linkUrl: 'shop/jackets'
         },
         {
             title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            imageUrl: 'https://i.ibb.co/sWpykpf/sneakers.jpg',
             id: 3,
             linkUrl: 'shop/sneakers'
         },
         {
             title: 'womens',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            imageUrl: 'https://i.ibb.co/PMF2qgg/women.jpg',
             size: 'large',
             id: 4,
             linkUrl: 'shop/womens'
         },
         {
             title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            imageUrl: 'https://i.ibb.co/Ttntz8H/men.jpg',
             size: 'large',
             id: 5,
             linkUrl: 'shop/mens'
@@ -36,3 +36,19 @@ const DIRECTORY_DATA = {
 }
 
 export default DIRECTORY_DATA;
+
+// https://i.ibb.co/jrg1RZW/5-Closet-Staples-You-Need-For-Fall-my-kind-of-sweet.jpg
+// https://i.ibb.co/5K5y7dy/efa9f4cb068a91c8eb375b5d0fb21d8f.jpg
+// https://i.ibb.co/FXwBG16/Fall-Outfits-for-Men-17-Casual-Fashion-Ideas-This-Fall.jpg
+// 
+// 
+// https://i.ibb.co/19ChFJV/men.png
+// 
+// https://i.ibb.co/2NZJRLY/The-Stailee-Top.jpg
+// https://i.ibb.co/kgb1Mmh/women.png
+
+// https://i.ibb.co/5TnsPgY/81dab834200ca873fc84163b830273bf.jpg
+// https://i.ibb.co/phPCr6y/hats2.jpg
+// https://i.ibb.co/R2fNJ19/Baseball-Uniform-Coat-Male-Jaqueta-Masculino-Bomber.jpg
+// https://i.ibb.co/m6WwHHv/Fall-Outfits-for-Men-17-Casual-Fashion-Ideas-This-Fall.jpg
+// https://i.ibb.co/fXDFkVh/men3.jpg
